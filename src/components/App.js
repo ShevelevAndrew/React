466140f4392d627ad8b6f44971bcbe847e7e1594
Andrew <sit-surgut@mail.ru>
2021-03-25 11:30:21 +0500

    hw_add

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Counter from './Counter'
+
+function App() {
+    return (
+        <div>
+            <h1>Hello react</h1>
+            <Counter />
+        </div>
+    )
+}
+
+export default App
